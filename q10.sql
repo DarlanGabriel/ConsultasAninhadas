101d772acd76c74973f6551d3588c56adfb5a5c8
Darlan Gabriel de Medeiros Macêdo <darlan.acari@gmail.com>
2020-10-06 22:21:58 -0300

    Create q10.sql

---
+++ q10.sql
@@ -0,0 +1,5 @@
+Select p.Nome, d.Nome, f.Nome, e.Nome
+From Projeto p, Departamento d
+Where p.CodDepto = d.Codigo
+Left Join Funcionario f, Departamento e
+Where p.CodResponsavel = f.Codigo AND e.Codigo = f.CodDepto;
